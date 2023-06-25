905d1722976fd7d6de0ecc51063f40f998e94489
GaryFrZero <garyesp@hotmail.com>
2023-06-25 17:21:28 -0500

    Diseño Dashboard con su Path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from './angular-material/material.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,8 +12,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path:'dashboard', component:DashboardComponent
+    
+      },
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+      
+      
+    ])
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
